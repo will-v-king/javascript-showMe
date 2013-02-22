98bd1e57a0976945119afcd2a16c32d6b318ba27
will <will-v-king@hotmail.com>
2013-02-22 11:21:45 +0800

    Create getStr.js
    getStr is a function analyze json object and output a string description by burning memory. If the object is too large, or the memory is too small, this function won't work it out.

---
+++ getStr.js
@@ -0,0 +1,82 @@
+// 将字符串 数组 对象 以字符串的形式返回。
+if (!getStr) {
+        var getStr = function(obj) {
+		var str = '';
+		if (typeOf(arguments[1], 'String')) {
+			var argumentsObj = {};
+			argumentsObj[arguments[1]] = true;
+			arguments[1] = argumentsObj;
+		}
+		arguments[1] = arguments[1] || {};
+		var getStrLevel = arguments[1].getStrLevel = arguments[1].getStrLevel || 1;
+		var maxDepth = arguments[1].maxDepth;
+		if (typeOf(maxDepth, 'number') && getStrLevel++ > maxDepth) {
+			return '...';
+		}
+		var command = arguments[1] || {};
+		var typeValue = typeOf(obj);
+		switch (typeValue) {
+			case 'String':
+				str = str + '"' + String(obj) + '"';
+				break;
+			case 'Array':
+				var length = obj.length;
+				str = str + '[';
+				for (var i = 0; i < length; i++) {
+					str = str + getStr(obj[i], arguments[1]) + ',';
+				}
+				var lio = str.lastIndexOf(',');
+				lio = lio == - 1 ? str.length: lio;
+				str = str.substring(0, lio) + ']';
+				delete lio;
+				delete length;
+				break;
+			case 'Error':
+			case 'Object':
+				str = str + '{';
+				try {
+					for (var prop in obj) {
+						str = str + '"' + prop + '"' + ':' + getStr(obj[prop], arguments[1]) + ',';
+					}
+				} catch(e) {
+					if (!jQuery.isPlainObject(obj)) {
+						str = str + 'object';
+						break;
+					}
+				}
+				var lio = str.lastIndexOf(',');
+				lio = lio == - 1 ? str.length: lio;
+				str = str.substring(0, lio) + '}';
+				delete lio;
+				delete prop;
+				break;
+			case 'Number':
+				str = str + obj;
+				break;
+			case 'Function':
+				if (command == 'functionNameOnly' || command.functionNameOnly) {
+					str = str + 'function()';
+				} else {
+					str = str + String(obj);
+				}
+				break;
+			case 'Boolean':
+				str = str + String(obj);
+				break;
+			case 'RegExp':
+				str = str + String(obj);
+				break;
+			case 'Date':
+				str = str + (obj.toSource === undefined ? obj.toString() : obj.toSource());
+				break;
+			default:
+				str = str + '[object ' + typeValue + ']';
+				break;
+		}
+		delete typeValue;
+		delete command;
+		delete arguments;
+		return str;
+		delete str;
+	};
+}
